9deff97273637bae94a4ddd084643a1298aed9f3
ddderi <maxime830@live.fr>
2022-06-21 03:22:19 +0200

    commit tree 951c2b8815746ea6ced4cd7a0f485969f60e1452

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+        def initialize(word)
+        @word = word
+        end
+
+        def match(arrayofstring)
+            newarray = []
+           arrayofstring.each do |wor|
+            if wor.split("").sort == @word.split("").sort   
+                newarray << wor
+            end
+        end
+        newarray
+        end
+
+end
